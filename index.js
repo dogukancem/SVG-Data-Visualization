64e93bcc4b731d4e96fedab7aaf591bb8f2b0810
dogukancem <105238554+dogukancem@users.noreply.github.com>
2023-10-17 15:39:32 +0300

    Created line chart for suicide rates.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const data = [
   { "year": 2022, "number_of_suicides": 4196, "crude_suicide_rate": 4.88 }
 ]
 
-// SVG size settings
+// SVG size settings: Defines the dimensions and margins of the SVG element
 const svgWidth = 1100;
 const svgHeight = 600;
 const margin = { top: 50, right: 20, bottom: 50, left: 80 };
@@ -51,8 +51,8 @@ xAxisLabel.setAttribute("text-anchor", "middle");
 xAxisLabel.textContent = "Year";
 svg.appendChild(xAxisLabel);
 
-// Add data values to X-axis
-const xScale = d => (chartWidth / (2022 - 2001)) * (d - 2001) + margin.left + 10; // Dynamic scaling
+// Add data values to X-axis: Calculates and positions the year labels on the x-axis
+const xScale = d => (chartWidth / (2022 - 2001)) * (d - 2001) + margin.left + 10;
 data.forEach(d => {
   const xText = document.createElementNS("http://www.w3.org/2000/svg", "text");
   xText.setAttribute("x", xScale(d.year) + 10);
@@ -80,8 +80,10 @@ yAxisLabel.setAttribute("text-anchor", "middle");
 yAxisLabel.textContent = "Number of Suicides";
 svg.appendChild(yAxisLabel);
 
+// Max value for Y-axis. This will be used to scale the data points and grid lines
 const yMax = 4500;
 
+// Function to scale the y-values, making sure the bars fit within the chart dimensions
 const yScale = d => chartHeight + margin.top - (d / yMax) * chartHeight;
 
 // Add data values to Y-axis (0 to 4500, step by 500)
@@ -95,7 +97,7 @@ for (let i = 0; i <= 4500; i += 500) {
   svg.appendChild(yText);
 }
 
-// Draw bars
+// Draw bars: Loops through the data to create SVG rect elements for each bar in the bar chart
 data.forEach(d => {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttribute("x", xScale(d.year));
@@ -116,7 +118,82 @@ data.forEach(d => {
   svg.appendChild(yTextInsideBar);
 });
 
-// Add chart title
+// Draw Y-axis Secondary line
+const yAxisSecondaryLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+yAxisSecondaryLine.setAttribute("x1", margin.left + chartWidth + 50);
+yAxisSecondaryLine.setAttribute("y1", margin.top - 15);
+yAxisSecondaryLine.setAttribute("x2", margin.left + chartWidth + 50);
+yAxisSecondaryLine.setAttribute("y2", chartHeight + margin.top);
+yAxisSecondaryLine.setAttribute("stroke", "black");
+svg.appendChild(yAxisSecondaryLine);
+
+// Add "Number of Suicides" label to Y-axis
+const yAxisSecondaryLabel = document.createElementNS("http://www.w3.org/2000/svg", "text");
+yAxisSecondaryLabel.setAttribute("x", -chartHeight / 2 - margin.top);
+yAxisSecondaryLabel.setAttribute("y", margin.left + chartWidth + 110);
+yAxisSecondaryLabel.setAttribute("transform", "rotate(-90)");
+yAxisSecondaryLabel.setAttribute("text-anchor", "middle");
+yAxisSecondaryLabel.textContent = "Crude Suicide Rate";
+svg.appendChild(yAxisSecondaryLabel);
+
+// Max value for secondary Y-axis. This will be used to scale the data points and grid lines
+const ySecondaryMax = 5.5;
+
+// Function to scale the secondary y-values, making sure they fit within the chart dimensions
+// Same logic as yScale
+const ySecondaryScale = d => chartHeight + margin.top - (d / ySecondaryMax) * chartHeight;
+
+// Add data values to Secondary Y-axis (0 to 5.5, step by 0.5)
+for (let i = 0; i <= 5.5; i += 0.5) {
+  // Draw text labels for the secondary y-values
+  const ySecondaryText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  ySecondaryText.setAttribute("x", margin.left + chartWidth + 80);
+  ySecondaryText.setAttribute("y", ySecondaryScale(i) + 5);
+  ySecondaryText.setAttribute("text-anchor", "end");
+  ySecondaryText.textContent = i;
+  svg.appendChild(ySecondaryText);
+}
+
+// Function to generate SVG path data for the line chart
+const createPathData = () => {
+  // Initialize an empty string to store the path data
+  let pathData = "";
+
+  // Loop through each data point
+  for (let i = 0; i < data.length; i++) {
+    // Calculate the x-coordinate using the scaled year value. Add 12 to make it adjusted correctly.
+    const x = xScale(data[i].year) + 12;
+
+    // Calculate the y-coordinate using the scaled crude_suicide_rate value
+    const y = ySecondaryScale(data[i].crude_suicide_rate);
+
+    // Create the path data string
+    // If it's the first point, use "M" (Move to) to start the path
+    // Otherwise, use "L" (Line to) to extend the line to the next point
+    pathData += i === 0 ? `M${x},${y} ` : `L${x},${y} `;
+  }
+
+  // Return the generated path data string
+  return pathData;
+};
+
+// Create an SVG 'path' element to draw the line chart
+const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+// Generate the SVG path data using the createPathData function
+const pathData = createPathData();
+
+// Set attributes for the SVG 'path' element
+// 'd' specifies the path data (coordinates for drawing the line)
+pathElement.setAttribute("d", pathData);
+pathElement.setAttribute("stroke", "#FF5733"); // Line color
+pathElement.setAttribute("stroke-width", "2"); // Line thickness
+pathElement.setAttribute("fill", "none");
+
+// Append the 'path' element to the SVG container
+svg.appendChild(pathElement);
+
+// Add chart title: Centers and positions the title above the chart
 const title = document.createElementNS("http://www.w3.org/2000/svg", "text");
 title.setAttribute("x", svgWidth / 2);
 title.setAttribute("y", margin.top - 20);
